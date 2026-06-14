a2bc8d39d36d566e312ac4780f0d1795b70ac7ad
Soon-Ping Wang <spwang@MacBookAir.lan>
2026-06-14 12:22:54 -0600

    setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,6 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
 }
---
+++ app/src/main/java/Entities/Api/Karaoke.kt
@@ -0,0 +1,4 @@
+package Entities.Api
+
+class Karaoke {
+}
